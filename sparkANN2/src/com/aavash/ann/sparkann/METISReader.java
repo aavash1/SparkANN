b8fd4276768411d438cd725a6bed2ea68dbd71b6
aavash <aavash@mainMaster>
2022-03-21 18:07:15 +0900

    CustomPartitioner Updated

--- sparkANN2/src/com/aavash/ann/sparkann/METISReader.java
+++ sparkANN2/src/com/aavash/ann/sparkann/METISReader.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import java.util.HashMap;
-
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -17,6 +17,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import com.aavash.ann.sparkann.graph.CustomPartitioner;
 import com.aavash.ann.sparkann.graph.Utilsmanagement;
 
 import scala.Tuple2;
@@ -26,13 +27,18 @@ public class METISReader {
 	@SuppressWarnings("unchecked")
 	public static <T> void main(String[] args) throws IOException, IOException {
 
-		SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("Graph");
+		// SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("Graph");
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("Graph")
+				.set("spark.shuffle.service.enabled", "false").set("spark.driver.blockManager.port", "10026")
+				.set("spark.driver.port", "10027").set("spark.cores.max", "3").set("spark.executor.memory", "1G")
+				.set("spark.driver.host", "210.107.197.209").set("spark.shuffle.service.enabled", "false")
+				.set("spark.dynamicAllocation.enabled", "false").set("spark.shuffle.blockTransferService", "nio");
 		try (JavaSparkContext javaSparkContext = new JavaSparkContext(conf)) {
 			// SparkANN/METISGraph/MetisTinyGraph
-			 String metisInputGraph = "Metisgraph/metis.txt";
-			 String metisPartitionOutputFile = "PartitionDataset/metis.txt";
-			//String metisInputGraph = "Metisgraph/Tinygraph.txt";
-			//String metisPartitionOutputFile = "PartitionDataset/tg_part.txt";
+			String metisInputGraph = "Metisgraph/metis.txt";
+			String metisPartitionOutputFile = "PartitionDataset/metis.txt";
+			// String metisInputGraph = "Metisgraph/Tinygraph.txt";
+			// String metisPartitionOutputFile = "PartitionDataset/tg_part.txt";
 
 			// 1. Read METIS graph input
 			Map<Object, List<Integer>> metisGraph = new HashMap<Object, List<Integer>>();
@@ -68,12 +74,37 @@ public class METISReader {
 						(List<Integer>) metisGraph.values().toArray()[i]);
 				metisGraphWithPartitionIndex.put(partitionIndex.get(i), nodesAndNeighbors);
 			}
-			
-			System.out.println(metisGraphWithPartitionIndex);
+
+			// System.out.println("The size of metisGraphWithPartition is: " +
+			// metisGraphWithPartitionIndex.size());
 
 			// 4. Creating an RDD of the metisGraphWithPartitionIndex.
-			List<Tuple2<Object, Map<Integer, List<Integer>>>> mapMetisGraphWithPartitionIndexR = new ArrayList<>(
+			List<Tuple2<Object, Map<Integer, List<Integer>>>> mapMetisGraphWithPartitionIndex = new ArrayList<>(
 					metisGraphWithPartitionIndex.size());
+
+			// 4.1 Converting the HashMap to scala.Tuple2 and adding object to the list.
+			for (Map.Entry<Object, Map<Object, List<Integer>>> i : metisGraphWithPartitionIndex.entries()) {
+				mapMetisGraphWithPartitionIndex.add(new Tuple2(i.getKey(), i.getValue()));
+			}
+
+			// 4.2 Creating a JavaPairRDD.
+			JavaPairRDD<Object, Map<Integer, List<Integer>>> metisGraphWithPartitionIndexRDD = javaSparkContext
+					.parallelizePairs(mapMetisGraphWithPartitionIndex);
+
+			// metisGraphWithPartitionIndexRDD.foreach(x -> System.out.println(x._1() + " "
+			// + x._2()));
+
+			JavaPairRDD<Object, Map<Integer, List<Integer>>> customPartitioned = metisGraphWithPartitionIndexRDD
+					.partitionBy(new CustomPartitioner(2));
+
+			JavaRDD<Object> customPartitionedIndex = customPartitioned
+					.mapPartitionsWithIndex((index, tupleIterator) -> {
+						List<Object> list = new ArrayList<>();
+						while (tupleIterator.hasNext()) {
+							list.add("Partition number: " + index + " ,key: " + tupleIterator.next()._1());
+						}
+						return list.iterator();
+					}, true);
 
 		}
 
--- sparkANN2/src/com/aavash/ann/sparkann/graph/CustomPartitioner.java
+++ sparkANN2/src/com/aavash/ann/sparkann/graph/CustomPartitioner.java
@@ -3,18 +3,22 @@ package com.aavash.ann.sparkann.graph;
 import org.apache.spark.Partitioner;
 
 public class CustomPartitioner extends Partitioner {
-	final int maxPartitions = 2;
+	private final int numParts;
+
+	public CustomPartitioner(int i) {
+		numParts = i;
+	}
 
 	@Override
 	public int getPartition(Object key) {
-		// TODO Auto-generated method stub
-		return ((((String) key).length()) % maxPartitions);
+		int partIndex = ((Integer) key);
+		return partIndex;
 	}
 
 	@Override
 	public int numPartitions() {
 		// TODO Auto-generated method stub
-		return maxPartitions;
+		return numParts;
 	}
 
 }
